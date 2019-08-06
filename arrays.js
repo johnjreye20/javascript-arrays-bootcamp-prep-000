51d522c3904b8b29cbb89d787f2ac9d87bfa9c1b
John Reyes <john.jreye@gmail.com>
2019-08-06 23:47:48 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"]; 
+ 
+ console.log(chocolateBars);
+var array = addElementToBeginningOfArray, destructivelyAddElementToBeginningOfArray;
+
+var addElementToBeginningOfArray = [
+  "HTML",
+  "CSS",
+  "Javascript"];
+  
+var destructivelyAddElementToBeginningOfArray = [
+  "Web Development",
+  "Software Engineer",
+  "Data Science"];
+  
+ console.log(addElementToBeginningOfArray,destructivelyAddElementToBeginningOfArray);
+  
